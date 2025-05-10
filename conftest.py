0a3eac230742311dd883bf88d5556088fa7881e5
unknown <bagaidenis@gmail.com>
2025-05-10 19:48:02 +0300

    fixed according to the comments

--- conftest.py
+++ conftest.py
@@ -6,29 +6,33 @@ from main import BooksCollector
 def books_collector():
     return BooksCollector()
 
+
 @pytest.fixture
-def books_collector_w_one_book_name():
-    collector = BooksCollector()
+def books_collector_w_one_book_name(books_collector):
+    collector = books_collector
     collector.add_new_book(data.books_name[0])
     return collector
 
+
 @pytest.fixture
-def books_collector_w_one_book_and_genre():
-    collector = BooksCollector()
+def books_collector_w_one_book_and_genre(books_collector):
+    collector = books_collector
     collector.add_new_book(data.books_name[0])
     collector.set_book_genre(data.books_name[0], data.books_genre[1])   
     return collector
 
+
 @pytest.fixture
-def books_collector_w_one_book_and_genre_for_children():
-    collector = BooksCollector()
+def books_collector_w_one_book_and_genre_for_children(books_collector):
+    collector = books_collector
     collector.add_new_book(data.books_name[2])
     collector.set_book_genre(data.books_name[2], data.books_genre[3])   
     return collector
 
+
 @pytest.fixture
-def books_collector_w_one_book_name_in_favorites():
-    collector = BooksCollector()
+def books_collector_w_one_book_name_in_favorites(books_collector):
+    collector = books_collector
     collector.add_new_book(data.books_name[0])
     collector.add_book_in_favorites(data.books_name[0])
     return collector
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from main import BooksCollector
 import data
+import pytest
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -23,10 +24,13 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
-    def test_add_new_book_add_one_book(self, books_collector):
+    @pytest.mark.parametrize(
+        "book_name,result", ([data.books_name[0], 1], [data.books_name[1], 0])
+    )
+    def test_add_new_book_added_one_book(self, books_collector, book_name, result):
         collector = books_collector
-        collector.add_new_book("Гордость и предубеждение и зомби")
-        assert len(collector.books_genre) == 1
+        collector.add_new_book(book_name)
+        assert len(collector.books_genre) == result
 
     def test_set_book_genre_assign_genre_to_book(self, books_collector_w_one_book_name):
         collector = books_collector_w_one_book_name
@@ -43,15 +47,15 @@ class TestBooksCollector:
         collector = books_collector_w_one_book_and_genre
         assert len(collector.get_books_with_specific_genre(data.books_genre[1])) == 1
 
-    def test_get_books_genre_success(self, books_collector):
-        collector = books_collector
-        assert len(collector.genre) == 5
+    def test_get_books_genre_success(self, books_collector_w_one_book_and_genre):
+        collector = books_collector_w_one_book_and_genre
+        assert len(collector.get_books_genre()) == 1
 
     def test_get_books_for_children_success(
         self, books_collector_w_one_book_and_genre_for_children
     ):
         collector = books_collector_w_one_book_and_genre_for_children
-        assert len(collector.get_books_for_children()) > 0
+        assert len(collector.get_books_for_children()) == 1
 
     def test_add_book_in_favorites_success(self, books_collector_w_one_book_name):
         collector = books_collector_w_one_book_name
